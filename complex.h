c8977b25c84a6a7f2790e7c436297042621555f2
MaorMassas <164793395+MaorMassas@users.noreply.github.com>
2025-01-02 00:12:29 +0200

    complex.h: - Declare the prototype of the function. - Declare on the typedef struct. - Declare on the variable of complex.

--- complex.h
+++ complex.h
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+
+typedef struct complex{
+    double real;
+    double image;
+}complex;
+
+complex *A,*B,*C,*D,*E,F;
+
+read_mat(double real,double image);
+print_comp(complex X);
+add_comp(complex X,complex Y);
+sub_comp(complex X,complex Y);
+mult_comp_real(complex X,double real);
+mult_comp_img(complex X,double image);
+mult_comp_comp(complex X,complex Y);
+abs_comp(complex X);
