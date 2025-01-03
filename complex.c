6125f64eb1a846ccdb1b0d07c7e7f396351e89af
MaorMassas <164793395+MaorMassas@users.noreply.github.com>
2025-01-03 11:57:34 +0200

    complex.c: - Trying to modify and fulfill the function.

--- complex.c
+++ complex.c
@@ -1,5 +1,6 @@
 #include "complex.h"
 #include <stdio.h>
+#include <math.h>
 
 void read_comp(complex *X, double *real,double *image)
 {
@@ -9,7 +10,7 @@ void read_comp(complex *X, double *real,double *image)
 
 void print_comp(complex *X)
 {
-    printf("%lf + (%lf)i",X->real,X->image);
+    printf("%.2f + (%.2f)i",X->real,X->image);
 }
 
 void add_comp(complex *X,complex *Y)
@@ -23,25 +24,44 @@ void add_comp(complex *X,complex *Y)
 
 complex sub_comp(complex *X,complex *Y)
 {
+    complex tmp;
+    tmp.real = X->real - Y->real;
+    tmp.image = X->image - Y->image;
 
+    print_comp(&tmp);
 }
 
 complex mult_comp_real(complex *X,double *real)
 {
+    complex tmp;
+    tmp.real = X->real * *real;
+    tmp.image = X->image;
 
+    print_comp(&tmp);
 }
 
 complex mult_comp_img(complex *X,double *image)
 {
+    complex tmp;
+    tmp.image = X->image * *image;
+    tmp.real = X->real;
 
+    print_comp(&tmp);
 }
 
 complex mult_comp_comp(complex *X,complex *Y)
 {
+    complex tmp;
+    tmp.real = X->real * Y->real;
+    tmp.image = X->image * Y->image;
 
+    print_comp(&tmp);
 }
 
 complex abs_comp(complex *X)
 {
+    double result;
+    result = sqrt(pow(X->image,2) + pow(X->real,2));
 
+    printf("The result of abs is: %.2f\n", result);
 }
