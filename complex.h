4539fd87a1df6d1c25d239eafc9481fb8a386d69
MaorMassas <164793395+MaorMassas@users.noreply.github.com>
2025-01-03 11:59:48 +0200

    complex.c: - fixed some.

--- complex.h
+++ complex.h
@@ -8,11 +8,11 @@ typedef struct complex{
 
 complex *A,*B,*C,*D,*E,F;
 
-void read_comp(complex *X,double *real,double *image);
+void read_comp(complex *X,const double *real,const double *image);
 void print_comp(complex *X);
 void add_comp(complex *X,complex *Y);
 complex sub_comp(complex *X,complex *Y);
-complex mult_comp_real(complex *X,double *real);
-complex mult_comp_img(complex *X,double *image);
+complex mult_comp_real(complex *X,const double *real);
+complex mult_comp_img(complex *X,const double *image);
 complex mult_comp_comp(complex *X,complex *Y);
 complex abs_comp(complex *X);
